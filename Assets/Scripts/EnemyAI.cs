a7e6cb44d8adfa473bee86aa6a6aaeb97d16494d
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-09-08 13:49:14 -0400

    Added weapon for player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,7 +7,11 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 15f;
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +22,40 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if(isProvoked){
+            EngageTarget();
+        }else if(chaseRange > distanceToTarget){
+            isProvoked = true;
+        }
+        
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }else if(distanceToTarget < navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Destroy(target.gameObject);
+    }
+
+    private void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
