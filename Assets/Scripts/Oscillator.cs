5a055981fe2a69d2eced4b91673f2dec67ebe021
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-10-02 16:47:29 -0400

    Created Moving Platforms and Scripts

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(0,20f,0);
+    [SerializeField] float speed = 2f;
+
+    Vector3 startingPos;
+
+    const float tau = Mathf.PI * 2f;
+
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speed <= Mathf.Epsilon)
+        {
+            movementFactor = 0;
+        }
+        else
+        {
+            float cycles = Time.time / speed;
+            float rawSinWave = Mathf.Sin(cycles * tau);
+            movementFactor = rawSinWave / 2f + 0.5f;
+        }
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
---
+++ Assets/Scripts/PlatformTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour
+{
+    GameObject Player;
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            StartCoroutine(JumpOff());
+        }
+    }
+
+    IEnumerator JumpOff()
+    {
+        yield return new WaitForSeconds(0.6f);
+        Player.transform.parent = null;
+    }
+}
