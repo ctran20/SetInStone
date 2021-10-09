3b5354fa055e6663a2bfdb3a7b6011392be3a63c
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-10-08 22:13:51 -0400

    Projectile fly toward the player

---
+++ Assets/Scripts/EnemyFire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+{
+    [SerializeField] int MoveSpeed = 5;
+    Vector3 target;
+
+    int MinDist = 0;
+
+    private void Start()
+    {
+        transform.LookAt(FindObjectOfType<PlayerHealth>().transform);
+        target = FindObjectOfType<PlayerHealth>().transform.position;
+    }
+
+    void Update()
+    {
+
+            if (Vector3.Distance(transform.position, target) >= MinDist)
+            {
+
+                transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject, 0.05f);
+    }
+}
---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [SerializeField] private float delay = 1f;
+    [SerializeField] private float offsetX;
+    [SerializeField] private float offsetY;
+    [SerializeField] private float offsetZ;
+    [SerializeField] private float rotOffsetX;
+    [SerializeField] private float rotOffsetY;
+    [SerializeField] private float rotOffsetZ;
+    [SerializeField] private float timer;
+    [SerializeField] private GameObject spawnObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        Instantiate(spawnObject, new Vector3(transform.position.x + offsetX,
+                                            transform.position.y + offsetY,
+                                            transform.position.z + offsetZ),
+                                            new Quaternion(rotOffsetX, rotOffsetY, rotOffsetZ, 0));
+        Invoke("Spawn", delay);
+    }
+}
