fd55c06989eb5c527b56354288683733197df3a8
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-11-14 13:32:32 -0500

    Lava damage you and Wave knock you back

---
+++ Assets/Scripts/DangerZone.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerZone : MonoBehaviour
+{
+    GameObject player;
+    bool coolDown = false;
+    bool entered = false;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerHealth>().gameObject;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            entered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            entered = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (!coolDown && entered)
+        {
+            player.GetComponent<PlayerHealth>().TakeDamage(5f);
+            StartCoroutine(Damage());
+        }
+    }
+
+    IEnumerator Damage(){
+        coolDown = true;
+        yield return new WaitForSeconds(0.5f);
+        coolDown = false;
+    }
+}
