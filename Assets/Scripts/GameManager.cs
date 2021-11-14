1ad12b597a24667e68490d3f568a95d3ac3a8af1
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-11-14 17:21:54 -0500

    Set up trigger zone for transition

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,13 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject playerA2;
     [SerializeField] GameObject playerA3;
 
+    [SerializeField] GameObject area1;
+    [SerializeField] GameObject area2;
+    [SerializeField] GameObject area3;
+    [SerializeField] GameObject area4;
+
+    [SerializeField] GameObject boss;
+
     int currentStage = 1;
 
     private void Awake()
@@ -23,8 +30,7 @@ public class GameManager : MonoBehaviour
         }
 
     }
-
-    private void Start()
+    void Start()
     {
         if(currentStage == 1){
             playerA1.SetActive(true);
@@ -38,12 +44,35 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void inArea2(){
-        currentStage = 2;
-    }
-
-    public void inArea3()
-    {
-        currentStage = 3;
+    public void areaTrigger(int box){
+        switch(box){
+            case 1:
+                //leaveArea1
+                area2.SetActive(true);
+                break;
+            case 2:
+                //enterArea2
+                currentStage = 2;
+                area1.SetActive(false);
+                break;
+            case 3:
+                //leaveArea2
+                area3.SetActive(true);
+                break;
+            case 4:
+                //enterArea3
+                currentStage = 3;
+                area2.SetActive(false);
+                boss.SetActive(true);
+                break;
+            case 5:
+                //leaveArea3
+                area3.SetActive(false);
+                area4.SetActive(true);
+                break;
+            default:
+                Debug.Log("Unknow area value: " + box);
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/TriggerArea.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerArea : MonoBehaviour
+{
+    GameObject gameManager;
+    [SerializeField] int triggerLabel = 0;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>().gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            gameManager.GetComponent<GameManager>().areaTrigger(triggerLabel);
+            Destroy(gameObject);
+        }
+    }
+}
