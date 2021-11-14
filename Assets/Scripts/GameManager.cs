bc2f4ccb5cf76d99266f4ce02a7d1664848ac920
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-11-14 15:18:48 -0500

    Created Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameObject instance;
+
+    [SerializeField] GameObject playerA1;
+    [SerializeField] GameObject playerA2;
+    [SerializeField] GameObject playerA3;
+
+    int currentStage = 1;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void Start()
+    {
+        if(currentStage == 1){
+            playerA1.SetActive(true);
+        }
+        else if (currentStage == 1)
+        {
+            playerA2.SetActive(true);
+        }
+        else{
+            playerA3.SetActive(true);
+        }
+    }
+
+    public void inArea2(){
+        currentStage = 2;
+    }
+
+    public void inArea3()
+    {
+        currentStage = 3;
+    }
+}
